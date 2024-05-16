d82fcd72c97f87b6ba98cb333df0ad24a2ae732f
Subhajit Ghosh <subhajitstd07@gmail.com>
2024-05-16 23:07:04 +0530

    Initialized METAMASK clone project

---
+++ wallet-frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+import SignUp from "@/components/authcomponents/SignUp";
+
+export default function Home() {
+  return (
+    <section className="bg-white min-h-[42rem] flex justify-center items-center">
+      <SignUp />
+    </section>
+  );
+}
